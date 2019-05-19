44b1023107777fcdf98aa706fea6ce0174aaf315
Felipe Brasil <felbrasil@gmail.com>
2019-05-19 13:35:36 -0300

    Add componente slick para rotetor dos heróis, em desenvolvimento

---
+++ assets/js/slick/slick-home.js
@@ -0,0 +1,31 @@
+window.onload=function(){
+
+$(document).ready(function() {
+$(".testimonial-carousel").slick({
+    infinite: !0,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: !1,
+    arrows:true,
+    prevArrow: $(".testimonial-carousel-controls .prev"),
+    nextArrow: $(".testimonial-carousel-controls .next"),
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 3
+        }
+    }, {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 2
+        }
+    }, {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1
+        }
+    }]
+});
+});
+
+}
